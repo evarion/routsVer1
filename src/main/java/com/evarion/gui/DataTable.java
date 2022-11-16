ffd0bddcec612297f08a9f3d2eeedbb2738eed1d
Aleksandr <gmreloj@gmail.com>
2022-11-16 13:59:48 +0300

    DataTable

--- src/main/java/com/evarion/gui/DataTable.java
+++ src/main/java/com/evarion/gui/DataTable.java
@@ -8,16 +8,18 @@ public class DataTable extends JFrame {
 
     // Данные для таблиц
     private static Object[][] array = new String[][]{
-            {"1", "MEDU1329432", "SPB", "20.02.22","24.02.22", "MSC ATLANTIC", "NEW YORK"},
-            {"2", "MEDU1323116", "SPB", "20.02.22","24.02.22", "MSC ATLANTIC", "NEW YORK"},
-            {"3", "MEDU3452217", "SPB", "22.02.22","25.02.22", "VLADIMIR", "NEW YORK"}};
+            {"1", "MEDU1329432", "SPB", "20.02.22", "24.02.22", "MSC ATLANTIC", "NEW YORK"},
+            {"2", "MEDU1323116", "SPB", "20.02.22", "24.02.22", "MSC ATLANTIC", "NEW YORK"},
+            {"3", "MEDU3452217", "SPB", "22.02.22", "25.02.22", "VLADIMIR", "NEW YORK"}};
     // Заголовки столбцов
-    private static Object[] columnsHeader = new String[]{"NUM", "CONTAINER", "POL", "ATA POL","ATD POL", "VESSEL", "POD"};
+    private static Object[] columnsHeader = new String[]{"NUM", "CONTAINER", "POL", "ATA POL", "ATD POL", "VESSEL", "POD"};
 
     public static void constructDataTable() {
         JTable table1 = new JTable(array, columnsHeader);
         FrameMain.jVisiblePanelDataLeft.setLayout(new GridBagLayout());
-
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.weighty = 100; //ok
+        constraints.anchor = GridBagConstraints.NORTH;//ok
 
 
         // Данные для таблицы на основе Vector
@@ -38,7 +40,7 @@ public class DataTable extends JFrame {
         contents.add(new JScrollPane(table1));
 
         table1.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-        FrameMain.jVisiblePanelDataLeft.add(contents);
+        FrameMain.jVisiblePanelDataLeft.add(contents, constraints);
     }
 
     /*Box contents = new Box(BoxLayout.Y_AXIS);
