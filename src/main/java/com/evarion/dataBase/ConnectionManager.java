71e532aa4602374e98642cacace2f465df87b264
Aleksandr <gmreloj@gmail.com>
2023-01-22 18:48:38 +0300

    Update connection PostgreSQL

--- src/main/java/com/evarion/dataBase/ConnectionManager.java
+++ src/main/java/com/evarion/dataBase/ConnectionManager.java
@@ -1,41 +1,41 @@
 package com.evarion.dataBase;
 
 import java.sql.Connection;
+import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Scanner;
 
-public final class ConnectionManager {
 
-    private static final String PASSWORD = "24test1986";
-    private static final String USERNAME = "root";
+public class ConnectionManager {
+
     private static final String URL = "jdbc:postgresql://localhost:5432/routs";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "24test1986";
 
     public static void main(String[] args) {
-        openConnection();
-    }
-
-    public static void openConnection() {
         Connection connection;
+
         try {
-            try {
-                Class.forName("org.postgresql.Driver");
-            } catch (ClassNotFoundException e) {
-                throw new RuntimeException(e);
-            }
-            System.out.println("Драйвер подключен");
+            Driver driver = new org.postgresql.Driver();
+            DriverManager.registerDriver(driver);
+
             connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            if (connection != null) {
-                System.out.println("Соединение установлено");
-            } else {
-                System.out.println("Соединение не установлено");
+            if (!connection.isClosed()) {
+                System.out.println("соединение с базой данных установлено");
             }
+
+            System.out.println("введите число для закрытия соединения");
+            Scanner sc = new Scanner(System.in);
+            sc.nextInt();
+
+            connection.close();
+            if (connection.isClosed()) {
+                System.out.println("соединение с базой данных закрыто");
+            }
+
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            System.err.println("не удалось загрузить дравйвер");
         }
     }
 }
-
-
-
-
-
